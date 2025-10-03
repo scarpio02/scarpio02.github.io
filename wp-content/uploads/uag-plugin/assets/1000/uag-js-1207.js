d4f5a9ad34e9d2f441633523aa702aab4b48af47
scarpio02 <scarpio@ufl.edu>
2025-10-03 15:40:25 -0400

    Added static site files

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1207.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"4d5b562c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"santiagojcarpio@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Contact Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-4d5b562c', 1207 );
+});
+ });
